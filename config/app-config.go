de8eb0518d14adb8d47af30204dabfdadfbb0a47
John Doe <johndoe@example.com>
2023-09-09 23:18:30 +0100

    changed token maker

--- config/app-config.go
+++ config/app-config.go
@@ -14,7 +14,7 @@ type Config struct {
 	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	Port                 string        `mapstructure:"PORT"`
-	TokenKey             string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 }
